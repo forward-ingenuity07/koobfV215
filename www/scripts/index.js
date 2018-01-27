7a148e2a5c47cfe8aaa74e4260e395cef08e4a7c
Chipili Kafwilo <forward.ingenuity07@gmail.com>
2018-01-27 16:18:04 +0200

    first commit

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -76,7 +76,7 @@ function getIds() {
 // For Intel XDK and please add this to your app.js.
 
 document.addEventListener('deviceready', function () {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   /* (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
@@ -90,7 +90,7 @@ ga(function (tracker) {
     localStorage.setItem(GA_LOCAL_STORAGE_KEY, tracker.get('clientId'));
 });
 ga('send', 'pageview');
- 
+ */
     // console.log(ga.q)
     // Enable to debug issues.
     // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
